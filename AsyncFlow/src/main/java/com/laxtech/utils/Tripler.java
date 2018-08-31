45e63665ba5667bc39548655083dcba36b8d9d3b
Sanjeev <gsanjeevtripathi@gmail.com>
2018-08-31 18:24:57 +0530

    Java Mudule New

---
+++ AsyncFlow/src/main/java/com/laxtech/utils/Tripler.java
@@ -0,0 +1,21 @@
+package com.laxtech.utils;
+
+public class Tripler {
+	
+	private int a;
+
+	public Tripler() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int triple() {
+		return a*3;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+	
+	
+
+}
