3b0b2b8a9599ec20c90ace3acfe69c607334f82a
Sanjeev <gsanjeevtripathi@gmail.com>
2018-10-08 00:00:36 +0530

    spring-module-db-example and oauth provider

---
+++ spring-module-db-example/src/main/resources/db/sql/create-db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE books (
+  id         INTEGER PRIMARY KEY,
+  name VARCHAR(50),
+  author  VARCHAR(30)
+);
---
+++ spring-module-db-example/src/main/resources/db/sql/insert-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO books VALUES (1, 'Paradise Lost', 'John Milton');
+INSERT INTO books VALUES (2, 'Paradise Regained', 'John Milton');
+INSERT INTO books VALUES (3, 'Hamlet', 'Shakespeare');
